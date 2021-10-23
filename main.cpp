9051bd1c40681b495de25c17b277842ffe667377
mohammad <benyaminbusiness81@gmail.com>
2021-10-23 11:00:43 +0330

    full program added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int n1=0,n2=1,n3,number;
+    cout<<"Enter the number : ";
+    cin>>number;
+    cout<<n1<<" "<<n2<<" ";
+    for(int i=2;i<number;++i)
+    {
+        n3=n1+n2;
+        cout<<n3<<" ";
+        n1=n2;
+        n2=n3;
+    }
 }
